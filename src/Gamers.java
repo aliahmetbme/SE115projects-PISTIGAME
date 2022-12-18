d293f1ab47e5f4d351a89a6ed85bff0decb9fc80
aliahmetbme <082aliahmet4@gmail.com>
2022-12-18 04:11:28 +0300

    hazırladığım taslak üzerinden oyunu gerçek deste üzerinden yeniden planlayıp algoritmasını olusturmaya başladım . Stringlerde null ile ilgili sıkıntılar olduğu için bazı methodları ve kodlaru düzenlemek durumunda kaldım. Ana taslak üzerinden testler devam ediyor. Kart dağıtma ve deste güncelleme tekrar kontrol et, yorum satırları eklemesi yapılıyor.

---
+++ src/Gamers.java
@@ -0,0 +1,141 @@
+import java.util.Random;
+
+public class Gamers {
+    private String name;
+    private String[] cards;
+    private int point;
+
+    // CONSTRUCTIOR//
+
+    public Gamers(String NAME, String[] CARDS, int POINT){
+        setName(NAME);
+        setCards(CARDS);
+        setPoint(POINT);
+    }
+
+    // GETTER-SETTER //
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String[] getCards() {
+        return cards;
+    }
+
+    public void setCards(String[] cards) {
+        this.cards = cards;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    // METHODS //
+
+    public String[] mixDeck(String[] deck){
+        Random rd = new Random();
+//        int[] examplearrayim = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+//        While this class was created, this array was used as example
+
+        String[] mixedDeck = new String[deck.length];
+        int i = 0;
+        while (i < deck.length){
+            int new_index = rd.nextInt(deck.length);
+            if(mixedDeck[new_index] == null){ // if the index is not null do not put anything here
+                mixedDeck[new_index] = deck[i];
+                i++; // Each index fulled i increases and when i became deck.length - 1 the looped is finished
+            }
+        }
+        return mixedDeck; // it returns current deck
+    }
+
+    public String[] cutDeck(String[] deck, int x){
+        String[] firstPart = new String[x]; // first part before cutted
+        String[] secondPart = new String[deck.length - x]; // second part before cutted
+        String[] cuttedDeck = new String[deck.length]; // current deck after cutted
+
+        for (int i = 0 ; i < x ; i ++){ // first part is fulled to firstPart array
+            firstPart[i] = deck[i];
+        }
+        for (int i = 0 ; i < deck.length - x ; i++) { // second part is fulled to secondPart array
+            secondPart[i] = deck[i + x];
+        }
+        for (int i = 0 ; i < x ; i ++) { // first part is fulled to cuttedDeck array
+            cuttedDeck[i + secondPart.length] = firstPart[i];
+        }
+        for (int i = 0 ; i < deck.length - x ; i ++){ // second part is fulled to cuttedDeck array
+            cuttedDeck[i] = secondPart[i];
+        }
+        return cuttedDeck; // it returns current deck
+
+    }
+
+    public void separateCards(String[] gamer1, String[] gamer2 , String[] deck){
+        // The cards are dealt to a first player and dealt to a second player until the hands are full.
+        gamer1[0] = deck[0];
+        gamer2[0] = deck[1];
+        gamer1[1] = deck[2];
+        gamer2[1] = deck[3];
+        gamer1[2] = deck[4];
+        gamer2[2] = deck[5];
+        gamer1[3] = deck[6];
+        gamer2[3] = deck[7];
+
+    }
+
+    public String[] putCards_toBoard(String[] deck){
+        String[] cardsOnBoard = new String[4];
+
+        for (int i = 0 ; i < 4 ; i ++){
+            cardsOnBoard[i] = deck[i + 8];
+            // because we separate cards to players before this method firs 8 index will be null
+        }
+        return cardsOnBoard; // it returns current deck
+    }
+
+    public String showFirstCard(String[] puttedCards_toBoard){
+        return puttedCards_toBoard[0]; // in the game first card of cards that on the board is shown.
+    }
+
+    public String[] UpdateCurrentCards(String[] deck){ // this method updates the deck after cards are separated.
+        int amountOfCurrentCards ;
+
+        if (deck.length < 8){
+            amountOfCurrentCards = deck.length;
+        } else {
+            amountOfCurrentCards = deck.length - 8;
+        }
+        String[] CurrentCards = new String[amountOfCurrentCards];
+
+        for (int i = 0 ; i < deck.length - 9 ; i ++){
+            CurrentCards[i] = deck[9 + i];
+        }
+
+        return CurrentCards;
+    }
+
+    public void showHand(String[] hand){ // it shows cards that are on the hand of player
+        for (int i = 0 ; i < hand.length ; i ++){
+            System.out.println(hand[i]);
+        }
+    }
+
+    public boolean isSame(String cardHand , String card_on_board){
+        if (card_on_board == null){
+            return false;
+        } else {
+
+            return (cardHand.charAt(1) + "").equals(card_on_board.charAt(1) + "");
+        }
+
+    }
+}
---
+++ src/PlayGame.java
@@ -0,0 +1,220 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class PlayGame {
+    public static void main(String[] args) {
+        // Classes of Random and Scanner
+        Scanner scan = new Scanner(System.in);
+        Random rd = new Random();
+
+        // Creating Gamers
+
+        Gamers dealer = new Gamers("name",new String[4] , 0);
+        Gamers computer = new Gamers("Computer" , new  String[4] ,0);
+
+        String[] deck = {"#1","#2","#3","#4","#5","#6","#7","#8","#9","#10","#11","#12","#13",
+                         "$1","$2","$3","$4","$5","$6","$7","$8","$9","$10","$11","$12","$13",
+                         "%1","%2","%3","%4","%5","%6","%7","%8","%9","%10","%11","%12","%13",
+                         "&1","&2","&3","&4","&5","&6","&7","&8","&9","&10","&11","&12","&13"};
+        System.out.println("WELCOME THE GAME YOUR COMPETITOR WILL BE COMPUTER PLEASE ENTER YOUR NAME :");
+
+        String dealersName = scan.nextLine();
+        dealer.setName(dealersName);
+
+        // dealer mixes the deck
+        deck = dealer.mixDeck(deck);
+
+        // Computer cut the deck
+        int x = rd.nextInt(52); // Computer chooses where to cut the deck
+        deck  = computer.cutDeck(deck,x);
+
+        // Players' Hands
+        String[] dealersHand = dealer.getCards();
+        String[] computerHand = computer.getCards();
+
+        // Players' Points
+        int dealersPoint = dealer.getPoint();
+        int computerPoint = computer.getPoint();
+
+        // dealer separates cards
+        dealer.separateCards(dealersHand,computerHand,deck);
+
+        // after separating cards 4 cards are putted to board
+        String[] cardsOnBoard = dealer.putCards_toBoard(deck);
+
+        // first card on the board
+        String FirstCard = dealer.showFirstCard(cardsOnBoard);
+
+        // First card on the Board
+        System.out.println(FirstCard);
+        deck = dealer.UpdateCurrentCards(deck);
+
+        System.out.println("GAME STARTS WITH " + dealer.getName() );
+
+        int tur = 1;
+
+        // Game loop
+        while (true){
+            System.out.println(dealer.getName() + "'s hand ");
+            dealer.showHand(dealersHand);
+
+            String chosenCard = scan.nextLine(); // Gamer chooses card
+            boolean chosenCard_exist = true;
+
+            // this loop checks that is chosen card available in the hand or not
+            for (int i = 0 ; i < dealersHand.length ; i ++){
+                // if card is available in the hand statement will be false and do not go if statement
+                if (dealersHand[i] != chosenCard){
+                    // 9 10 11 12
+                    // 10
+                    chosenCard_exist = false;
+
+                }
+            }
+            // if it still true the card is not available on the hand
+            if (chosenCard_exist){
+                System.out.println("You did not choose correct card");
+                // the game will be finish
+                // new method can be developed
+                break;
+            }
+
+            // chosen card is same with card on the board
+            if (dealer.isSame(chosenCard,FirstCard)){
+                dealersPoint += 3; // dealer gain 3 points
+                dealer.setPoint(dealersPoint); // current point are assigned to dealers point
+                FirstCard = null;
+
+            } else {
+                FirstCard = chosenCard;
+            }
+
+            System.out.println(FirstCard + "aFTER PLAY KUROİYER"); // Computer must see card on the board
+            if (FirstCard == null){
+                System.out.println("....");
+            }
+
+            // this loop make null index that is same with chosenCard of Hand so next turns same cards does not be chosen.
+            for (int i = 0 ; i < dealersHand.length ; i++){
+                if (dealersHand[i] == null){ //if it is already null , this prevents any error
+                    continue;
+                }
+                if(dealersHand[i].equals(chosenCard)){
+                    dealersHand[i] = null;
+                }
+            }
+
+            System.out.println(computer.getName() + "' turn ");
+
+            boolean canIt_take_cards = false;
+            int index;
+
+            // this loop check whether the same card or index chosen more than one time or not
+            // if not chosen before , loop complete it works.
+            while (true){
+                index = rd.nextInt(4);
+                if (computerHand[index] != null) {
+                    System.out.println("Yes this did not be chosen go on ");
+                    break;
+                }
+                // this protects the code to go infinite
+                boolean isAll_null = false;
+
+                for (int i = 0 ; i < 4 ; i ++){
+                    if (computerHand[i] != null){
+                        // if one index of the hand is not null, the loop still can continue
+                        isAll_null = true;
+                    }
+                }
+                if (!isAll_null){
+                    break;
+                }
+            }
+//            if (computer.isSame(computerHand[index],FirstCard)){
+//                canIt_take_cards = true;
+//            }
+//            if(canIt_take_cards){
+//                computerPoint += 3;
+//                computer.setPoint(computerPoint);
+//                FirstCard = null;
+//            }else {
+//                FirstCard = computerHand[index];
+//            }
+            if (computer.isSame(computerHand[index],FirstCard)){
+                computerPoint += 3;
+                computer.setPoint(computerPoint);
+                FirstCard = null;
+            } else {
+                FirstCard = computerHand[index];
+            }
+
+            computerHand[index] = null; // chosen index will be null to prevent same card is chosen again
+
+            System.out.println(FirstCard);
+            if (FirstCard == null){
+                System.out.println("....");
+            }
+
+
+            boolean isAll_null_dealer = true;
+            boolean isAll_null_computer = true;
+
+            // this loop checks that whether all index are null or not
+            for (int i = 0 ; i < 4 ; i ++){
+                if (dealersHand[i] != null){
+                    isAll_null_dealer = false;
+                    break;
+                }
+                if (computerHand[i] != null){
+                    isAll_null_computer = false;
+                    break;
+
+                }
+            }
+            System.out.println(deck.length + " uzunluk " + " Tur " + tur);
+
+            //cards are less than 8, it means the game has to finished
+            if (deck.length < 5){
+                System.out.println("The Game is over ");
+                if(computer.getPoint() > dealer.getPoint()){
+                    System.out.println("computers point is " + computer.getPoint());
+                    System.out.println(dealer.getName() + " point is " + computer.getPoint());
+                    System.out.println("**************Computer won ***************");
+                } else if (computer.getPoint() < dealer.getPoint()) {
+                    System.out.println("computers point is " + computer.getPoint());
+                    System.out.println(dealer.getName() + " point is " + computer.getPoint());
+                    System.out.println("**************" + dealer.getName() + " !! You won ***************");
+
+                }else {
+                    System.out.println("computers point is " + computer.getPoint());
+                    System.out.println(dealer.getName() + " point is " + computer.getPoint());
+                    System.out.println("Everyone won");
+                }
+                break;
+            }
+
+            // if game is still continue, and all indexes are null; new cards has to be separated
+            if (isAll_null_computer && isAll_null_dealer){
+                System.out.println("alis hand");
+                for (int i = 0 ; i < dealersHand.length ; i ++ ){
+                    System.out.println(dealersHand[i]);
+                }
+                System.out.println("Computers hand");
+                for (int i = 0 ; i < dealersHand.length ; i ++ ){
+                    System.out.println(computerHand[i]);
+                }
+                dealer.separateCards(dealersHand,computerHand,deck); // cards are separated
+                System.out.println("alis hand");
+                for (int i = 0 ; i < dealersHand.length ; i ++ ){
+                    System.out.println(dealersHand[i]);
+                }
+                System.out.println("Computers hand");
+                for (int i = 0 ; i < dealersHand.length ; i ++ ){
+                    System.out.println(computerHand[i]);
+                }
+                System.out.println("card separated");
+                deck = dealer.UpdateCurrentCards(deck); // deck is updated
+            }
+        }
+    }
+}
